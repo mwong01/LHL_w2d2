4c4abce3501a63afa327225821ae2a939ce7f173
andyzen619 <andyzen619@gmail.com>
2019-10-22 17:22:47 +0000

    UPdate hello_timeout written by andy

--- hello_timeout.js
+++ hello_timeout.js
@@ -1,27 +1,28 @@
 //Andy Edit
 
-setTimeout(() => {
-  console.log("4311o th3r3 w0r1d");
-}, 3000);
-
-setTimeout(() => {
-  console.log("4311o");
-}, 1000);
-
-setTimeout(() => {
-  console.log("th3r3");
-}, 2000);
-
-setTimeout(() => {
-  console.log("w0r1d");
-}, 3000);
-
-
-// let str = "4311o th3r3 w0r1d";
-// let arr = str.split(" ");
-
-// for (word in arr) {
-// }
 // setTimeout(() => {
-//   console.log(arr[word]);
-// }, word * 1000);
+//   console.log("4311o th3r3 w0r1d");
+// }, 3000);
+
+// setTimeout(() => {
+//   console.log("4311o");
+// }, 1000);
+
+// setTimeout(() => {
+//   console.log("th3r3");
+// }, 2000);
+
+// setTimeout(() => {
+//   console.log("w0r1d");
+// }, 3000);
+
+
+let str = "4311o th3r3 w0r1d";
+let arr = str.split(" ");
+
+for (const word of arr) {
+
+  setTimeout(() => {
+    console.log(word);
+  }, 1000);
+}
