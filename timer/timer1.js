9f2a029c623072fa144b3eebdfc66c3d000d254b
andyzen619 <andyzen619@gmail.com>
2019-10-22 19:43:32 +0000

    Pull request: Added timer

--- timer/timer1.js
+++ timer/timer1.js
@@ -3,15 +3,16 @@ let arr = process.argv;
 let comm1 = arr[2];
 
 let comArr = arr.splice(2);
-comArr.sort(function(a,b) {
-  return a - b});
+comArr.sort(function(a, b) {
+  return a - b
+});
 
 
 for (let num of comArr) {
   let number = Number(num);
   if (number >= 0 && number !== NaN) {
-  setTimeout(() => {
-    process.stdout.write('\x07');
-  }, number * 1000)}
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, number * 1000)
+  }
 };
-
